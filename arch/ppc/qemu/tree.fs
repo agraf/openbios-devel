1b79d7b9e29b9a346eb401b388234ebf6c3399a0
Alexander Graf <agraf@suse.de>
2015-06-26 15:59:36 +0200

    XXX more hacks

--- arch/ppc/qemu/tree.fs
+++ arch/ppc/qemu/tree.fs
@@ -17,7 +17,8 @@ include config.fs
 [IFDEF] CONFIG_PPC64 2 [ELSE] 1 [THEN] encode-int " #address-cells" property
 1 encode-int " #size-cells" property
 h# 05f5e100 encode-int " clock-frequency" property
-h# 03003fff encode-int " AAPL,debug" property
+\ h# 03003fff encode-int " AAPL,debug" property
+\ h# 03000000 encode-int " AAPL,debug" property
 
 new-device
 	" cpus" device-name
